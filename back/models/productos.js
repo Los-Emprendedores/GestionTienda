f2fb8a59614b4039682783ba17073e55ebfcfcdd
Alejandro Baquero <diegobaquero991@gmail.com>
2022-11-24 14:11:47 -0500

    Update productos.js

--- back/models/productos.js
+++ back/models/productos.js
@@ -1,47 +1,47 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 
-const productosSchema=mongoose.Schema({
-    nombre:{
-        type:String,
-        required:[true,"Por favor registra el nombre del producto."],
-        trim:true,
-        maxLength:[120,"El nombre del producto no debe exceder los 120 caracteres."]
+const productosSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        required: [true, "Por favor registra el nombre del producto."],
+        trim: true,
+        maxLength: [120, "El nombre del producto no debe exceder los 120 caracteres."]
     },
-    precio:{
+    precio: {
         type: Number,
-        required:[true,"Por favor registre el precio del producto."],
-        maxLength:[8, "El precio del producto no puede estar por encima de 99'999.999"],
+        required: [true, "Por favor registre el precio del producto."],
+        maxLength: [8, "El precio del producto no puede estar por encima de 99'999.999"],
         default: 0.0
     },
-    descripcion:{
-      type:String,
-      required:[true,"Por favor registre una descripcion para el producto."]
+    descripcion: {
+        type: String,
+        required: [true, "Por favor registre una descripcion para el producto."]
     },
-      recomendacion:{
-        type:String,
-        required:[false,"Por favor ingrese una recomendación de consumo del producto."]
-      },
-    calificacion:{
+    recomendacion: {
+        type: String,
+        required: [false, "Por favor ingrese una recomendación de consumo del producto."]
+    },
+    calificacion: {
         type: Number,
         default: 0
     },
-    imagen:[
+    imagen: [
         {
-            public_id:{
-                type:String,
-                required:true
+            public_id: {
+                type: String,
+                required: true
             },
-            url:{
-                type:String,
-                required:true
+            url: {
+                type: String,
+                required: true
             }
         }
     ],
-    marca:{
-        type:String,
-        required:[true,"Por favor seleccione la marca del producto."],
-        enum:{
-            values:[
+    marca: {
+        type: String,
+        required: [true, "Por favor seleccione la marca del producto."],
+        enum: {
+            values: [
                 "Aguila",
                 "Poker",
                 "Becks",
@@ -56,47 +56,47 @@ const productosSchema=mongoose.Schema({
             ]
         }
     },
-    vendedor:{
-        type:String,
-        required:[true,"Por favor registre el vendedor de producto"]
+    vendedor: {
+        type: String,
+        required: [true, "Por favor registre el vendedor de producto"]
     },
-    inventario:{
+    inventario: {
         type: Number,
-        required:[true, "Por favor registre el stock del producto"],
-        maxLength:[5,"Cantidad maxima del producto no puede sobrepasar 99999"],
-        default:0
+        required: [true, "Por favor registre el stock del producto"],
+        maxLength: [5, "Cantidad maxima del producto no puede sobrepasar 99999"],
+        default: 0
     },
-    numCalificaciones:{
-        type:Number,
-        default:0
+    numCalificaciones: {
+        type: Number,
+        default: 0
     },
-    opiniones:[
+    opiniones: [
         {
-            nombreCliente:{
-                type:String,
-                required:true
+            nombreCliente: {
+                type: String,
+                required: true
             },
-            rating:{
-                type:Number,
-                required:true
+            rating: {
+                type: Number,
+                required: true
             },
-            comentario:{
-                type:String,
-                required:true
+            comentario: {
+                type: String,
+                required: true
             }
         }
     ],
-    user:{
+    user: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
         required: true
     },
 
-    fechaCreacion:{
-        type:Date,
-        default:Date.now
+    fechaCreacion: {
+        type: Date,
+        default: Date.now
     }
 
 })
 
-module.exports=mongoose.model("productos",productosSchema)
+module.exports = mongoose.model("productos", productosSchema)
